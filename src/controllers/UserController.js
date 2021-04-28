64192d278972ccbe4bc2c67e2b87168b14144962
Tatyana Fomina <tfomina@logrocon.com>
2021-04-28 17:50:59 +0300

    Доработки UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,11 @@
 const User = require("../models");
 
 module.exports = {
-  create(req, res) {
-    const data = req.body;
-
+  create(data) {
     return User.create(data).then((user) => user);
+  },
+
+  findByEmail(email) {
+    return User.findOne({ email: email }).then((user) => user);
   },
 };
