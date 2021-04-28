b9b855cda72f6f884a8e4e98c0d92ecc3ebf853e
Tatyana Fomina <tfomina@logrocon.com>
2021-04-28 11:41:32 +0300

    Базовая сборка проекта

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
