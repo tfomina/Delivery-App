37e83e9a99c8858a1ef9f05e373c16878c73db54
Tatyana Fomina <tfomina@logrocon.com>
2021-04-28 12:25:50 +0300

    Добавила миддлвары в index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
