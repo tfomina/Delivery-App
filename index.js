47853ee8b2dc37d3937730b822a4e7555187ee71
Tatyana Fomina <tfomina@logrocon.com>
2021-04-30 17:22:30 +0300

    Добавила подключение к MongoDB Atlas и пример .env файла

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const userApiRouter = require("./src/routes/user");
 const advertisementApiRouter = require("./src/routes/advertisement");
@@ -14,7 +15,25 @@ app.use(cors());
 app.use("/api", userApiRouter, advertisementApiRouter);
 
 const PORT = process.env.PORT || 3000;
+const UserDB = process.env.DB_USERNAME || "root";
+const PasswordDB = process.env.DB_PASSWORD || "AXRHV]cy?s/4UkZ";
+const NameDB = process.env.DB_NAME || "delivery_db";
+const HostDB = process.env.DB_HOST || "mongodb://localhost:27017/";
+const start = async () => {
+  try {
+    const UrlDB = `mongodb+srv://${UserDB}:${PasswordDB}@cluster0.m4q9c.mongodb.net/${NameDB}?retryWrites=true&w=majority`;
+    await mongoose.connect(encodeURI(UrlDB), {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
