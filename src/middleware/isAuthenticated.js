10d104834a2231a527c8a58382ae19c8b049052b
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-05-06 17:04:57 +0300

    Доработки

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
   if (!req.isAuthenticated || !req.isAuthenticated()) {
+    if (req.session) {
+      req.session.returnTo = req.originalUrl || req.url;
+    }
     return res.status(401).send({
       error: "Войдите или зарегистрируйтесь",
       status: "error",
