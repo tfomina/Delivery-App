68d44c4b29360d7346345765f4a97cd2a9aa712f
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-05-09 00:53:37 +0300

    доработки ChatSchema

--- src/models/chat.js
+++ src/models/chat.js
@@ -5,13 +5,15 @@ const { requiredMessage } = require("../helper");
 
 const ChatSchema = new Schema({
   users: {
-    type: [Schema.Types.ObjectId],
-    ref: "User",
+    type: [{ type: Schema.Types.ObjectId, ref: "User" }],
     required: [true, requiredMessage],
+    validate: [arraySize, "Должно быть 2 пользователя"],
   },
   createdAt: { type: Date, required: [true, requiredMessage] },
   messages: [MessageSchema],
 });
+
+const arraySize = (val) => val.length === 2;
 
 const Chat = mongoose.model("Chat", ChatSchema);
 
